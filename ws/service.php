d9c79ac3fff6b64cc86e125af1a4645c6bad0495
Alekuoshu <alejandro.villegas@farmalisto.com.co>
2019-03-07 14:02:51 -0500

    WS2 Ready

--- ws/service.php
+++ ws/service.php
@@ -1,14 +1,25 @@
 <?php
+require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'config'.DIRECTORY_SEPARATOR.'config.inc.php');
+require_once(dirname(__FILE__).'/../../../classes/order/Order.php');
+require_once(dirname(__FILE__).'/../../../classes/order/OrderHistory.php');
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 
 class Service
 {
  /**
   * Get data from picking
-  *
+    *
   * @return return array
   */
  public function pickingConfirmation($inPicking)
  {
+    //  instance this module
+    $module = Module::getInstanceByName('wsopenmarket');
+
 
   $fecha = $inPicking['Fecha'];
   $hora = $inPicking['Hora'];
@@ -39,6 +50,26 @@ if (preg_match("/^(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)$/",$hora)) {
 // 5 (006) – Entregado
 // 7 (007) – Devuelto
 // 15 (008) – Rechazado
+
+// chequear cada estado disponible
+if($inPicking['Estado'] == '003'){
+    $new_order_state = 3;
+}
+if ($inPicking['Estado'] == '004') {
+    $new_order_state = 14;
+}
+if ($inPicking['Estado'] == '005') {
+    $new_order_state = 4;
+}
+if ($inPicking['Estado'] == '006') {
+    $new_order_state = 5;
+}
+if ($inPicking['Estado'] == '007') {
+    $new_order_state = 7;
+}
+if ($inPicking['Estado'] == '008') {
+    $new_order_state = 15;
+}
 
 
   $pconfirmation = array(
@@ -75,12 +106,21 @@ switch ($inPicking['Estado']) {
         break;
 }
 
+// consultar si existe el pedido actual
+$db = Db::getInstance();
+$sql = 'SELECT current_state FROM '._DB_PREFIX_.'orders WHERE id_order = '.$inPicking["NroPedido"];
+$current_stateid = $db->getValue($sql);
+
+
 // Validar campos
 if ($inPicking['NroPedido'] == null) {
     $pconfirmation = '204 - El numero del pedido no es válido o está vacio';
 
 } elseif (!is_int($inPicking['NroPedido'])) {
     $pconfirmation = '406 - El numero de pedido no es entero';
+
+} elseif ($current_stateid < 1) {
+    $pconfirmation = '204 - El pedido con numero: '.$inPicking['NroPedido'].' no existe';
 
 } elseif ($fecha == null) {
     $pconfirmation = '204 - La fecha es nula';
@@ -97,6 +137,26 @@ if ($inPicking['NroPedido'] == null) {
 }
 
 
+// Actualizando estado
+if($current_stateid >= 1){
+    $order = new Order($inPicking['NroPedido']);
+    if (!Validate::isLoadedObject($order)) {
+        throw new PrestaShopException('No se ha podido cargar el estado de la orden');
+    }
+
+    $history = new OrderHistory();
+    $history->id_order = (int) $order->id;
+    $history->id_employee = 0;
+    $history->id_order_state = $new_order_state;
+     if ($history->save()) {
+        $history->changeIdOrderState($new_order_state, $order);
+         $module::logtxt('Estado de la orden '.$inPicking['NroPedido'].' actualizada a: ' . $new_order_state);
+     } else {
+         $module::logtxt('Error al completar la actualización del estado de la orden: ' . $inPicking['NroPedido']);
+     }
+
+}
+
 //   $pconfirmation = json_encode($pconfirmation, true);
   return array(
    'Estado' => utf8_decode($pconfirmation),
@@ -105,18 +165,3 @@ if ($inPicking['NroPedido'] == null) {
  }
 
 }
-
-
-
-// Db::getInstance()->insert('novonordisk_cart', array(
-//                 'id_cart' => $order->id_cart,
-//                 'id_order' => $order->id,
-//                 'cardnumber' => self::getCustomerCard($order->id_customer),
-//                 'transactionid' => self::getTransactionID($order->id_cart),
-//                 'saleauthnumber' => $result->saleauthnumber,
-//                 'errorid' => $result->errorid,
-//                 'sended' => '1',
-//                 'date_add' => date('Y:m:d H:i:s')
-//                     )
-//                     , false, true, Db::REPLACE);
-//         }
