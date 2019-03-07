e1b4d7c94e9335fa014553a6df68a8c48541a563
Alekuoshu <alekuoshu@gmail.com>
2019-03-07 06:13:37 -0500

    changes on WS2

--- ws/service.php
+++ ws/service.php
@@ -10,13 +10,32 @@ class Service
  public function pickingConfirmation($inPicking)
  {
 
-  $fecha = strtotime($inPicking['Fecha']);
-  $fecha = date('Y-m-d', $fecha);
+  $fecha = $inPicking['Fecha'];
+  $hora = $inPicking['Hora'];
+
+
+//  validar si la fecha es correcta
+if (preg_match("/^([0-2][0-9]|(3)[0-1])(-)(((0)[0-9])|((1)[0-2]))(-)\d{4}$/",$fecha)) {
+    $fechaCorrect = true;
+        $fecha = strtotime($inPicking['Fecha']);
+        $fecha = date('Y-m-d', $fecha);
+} else {
+    $fechaCorrect = false;
+}
+
+//  validar si la hora es correcta
+if (preg_match("/^(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)$/",$hora)) {
+    $horaCorrect = true;
+        $hora = strtotime($inPicking['Hora']);
+        $hora = date('Y-m-d', $hora);
+} else {
+    $horaCorrect = false;
+}
 
 // db id order states:
 // 3 (003) - Preparación en curso
 // 14 (004) – Empacado
-// 4 (005) – Enviado  
+// 4 (005) – Enviado
 // 5 (006) – Entregado
 // 7 (007) – Devuelto
 // 15 (008) – Rechazado
@@ -25,7 +44,7 @@ class Service
   $pconfirmation = array(
    'NroPedido' => $inPicking['NroPedido'],
    'Fecha' => $fecha,
-   'Hora' => $inPicking['Hora'],
+   'Hora' => $hora,
    'Estado' => $inPicking['Estado'],
   );
 
@@ -50,7 +69,7 @@ switch ($inPicking['Estado']) {
     case '008':
         $pconfirmation = '200 - Rechazado';
         break;
-    
+
     default:
         $pconfirmation = '204 - Debe enviar un codigo de estado válido!';
         break;
@@ -59,15 +78,22 @@ switch ($inPicking['Estado']) {
 // Validar campos
 if ($inPicking['NroPedido'] == null) {
     $pconfirmation = '204 - El numero del pedido no es válido o está vacio';
-    
+
 } elseif (!is_int($inPicking['NroPedido'])) {
     $pconfirmation = '406 - El numero de pedido no es entero';
 
-} elseif ($inPicking['Fecha'] == null) {
+} elseif ($fecha == null) {
     $pconfirmation = '204 - La fecha es nula';
+
+} elseif ($fechaCorrect == false) {
+    $pconfirmation = '204 - El formato de la fecha no es válido, ej: DD-MM-YYYY';
 
 } elseif ($inPicking['Hora'] == null) {
     $pconfirmation = '204 - La hora es nula';
+
+} elseif ($horaCorrect == false) {
+    $pconfirmation = '204 - El formato de la hora no es válido, ej: HH:MM:SS';
+
 }
 
 
